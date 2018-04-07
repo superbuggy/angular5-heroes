6c120a4f9ba39c6b7fdcfc9c342d660f134bd0ce
superbuggy <jpqreichard@gmail.com>
2018-04-07 16:01:18 -0400

    Put data fetching in an observational messaging service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes'
+// import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service'
 
 @Component({ //is a decorator function that specifies the Angular metadata for the component.
   selector: 'app-heroes',
@@ -12,16 +13,21 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero
 
-  heroes = HEROES
+  heroes: Hero[]
 
-  constructor () {
+  constructor (private heroService: HeroService) {
+  }
+
+  ngOnInit () {
+    this.getHeroes()
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes)
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero
   }
-
-  ngOnInit () {
-  }
-
 }
